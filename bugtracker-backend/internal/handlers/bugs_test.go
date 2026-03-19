5d20ec49cd16b14efcbb3a034f605d57d936011a
Willan Silisque Rojas <willan.eureka@gmail.com>
2026-03-19 17:15:48 -0300

    Making a test failed

--- bugtracker-backend/internal/handlers/bugs_test.go
+++ bugtracker-backend/internal/handlers/bugs_test.go
@@ -50,7 +50,7 @@ func TestCreateBug(t *testing.T) {
 				Description: "Test Description",
 			},
 			expectedStatus: http.StatusBadRequest,
-			expectedError:  "title is required",
+			expectedError:  "title is requiredXXX",
 		},
 		{
 			name:           "Invalid JSON",
