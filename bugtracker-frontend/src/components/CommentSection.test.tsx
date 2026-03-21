5722c07e59f4fe26662165eca0ab0a933cb1577a
Willan Silisque Rojas <willan.eureka@gmail.com>
2026-03-21 09:15:59 -0300

    Running E2E testes

--- bugtracker-frontend/src/components/CommentSection.test.tsx
+++ bugtracker-frontend/src/components/CommentSection.test.tsx
@@ -121,8 +121,7 @@ describe("CommentSection", () => {
       />
     );
 
-    expect(screen.getByText(/6\/10\/23.*(6|7):00:00/)).toBeInTheDocument();
-    // expect(screen.getByText(/6\/10\/23.*(10|11):00:00/)).toBeInTheDocument();
+    expect(screen.getByText(/6\/10\/23.*(10|11):00:00/)).toBeInTheDocument();
 
     jest.useRealTimers();
   });
