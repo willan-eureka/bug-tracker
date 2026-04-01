4df02bf9ef74b938ce01aa2a3c4507ebbc855a30
Willan Silisque Rojas <willan.eureka@gmail.com>
2026-04-01 10:54:54 -0300

    Deploy to fly.io with Jenkins

--- bugtracker-backend/cmd/bugtracker/main.go
+++ bugtracker-backend/cmd/bugtracker/main.go
@@ -72,6 +72,8 @@ func createServer() *http.Server {
 			"http://localhost:3000",
 			"https://bugtracker-staging-jameswillett.fly.dev",
 			"https://bugtracker-jameswillett.fly.dev",
+			"https://bugtracker-frontend-lively-flower-5051-will.fly.dev",
+			"https://bugtracker-frontend-staging-sil-will.fly.dev",
 		},
 		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders: []string{"*"},
